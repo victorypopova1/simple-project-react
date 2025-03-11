3a938f0fab21f8553a067ce4662e89cf928406fb
Victoria <victorypopova1@gmail.com>
2025-03-11 09:50:47 +0800

    fixed style

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
 import React, { useState } from 'react';
+import './App.css'; // Импорт CSS-файла
 
 function App() {
-    const [count, setCount] = useState(0);
+    const [countValue, setCountValue] = useState(0);
+
+    const incrementCount = () => {
+        setCountValue(countValue + 1);
+    };
+
+    const decrementCount = () => {
+        setCountValue(countValue - 1);
+    };
 
     return (
-        <div>
-            <h3>Первое приложение</h3>
-            <p>Количество: {count}</p>
-            <button onClick={() => setCount(count + 1)}>Увеличить</button>
+        <div className="app-container">
+            <div className="content-wrapper">
+                <h3>Первое приложение</h3>
+                <p>Количество: {countValue}</p>
+                <button style={{ margin: '5px' }} onClick={incrementCount}>
+                    Увеличить
+                </button>
+                <button style={{ margin: '5px' }} onClick={decrementCount}>
+                    Уменьшить
+                </button>
+            </div>
         </div>
     );
 }
